136ee8815fb2d7d9c0c20ff32b623f5846627ed2
Tanvi-Ghadai <ghadaitanvi00@gmail.com>
2025-12-13 15:19:23 +0530

    add

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import authRoutes from './routes/authRoutes.js';
-import eventRoutes from './routes/eventRoutes.js';
+
 import adminRoutes from './routes/adminRoutes.js';  
